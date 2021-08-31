e677e06b03ff9b537cea3ac59e765b3b838097b9
yanirmr <yanirmr@gmail.com>
2021-08-31 15:15:40 +0300

    set token holder

--- bot.py
+++ bot.py
@@ -50,7 +50,7 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater(TELEGRAM_TOKEN, use_context=True) #your token here
+    updater = Updater(os.environ['TELEGRAM_TOKEN'], use_context=True) #your token here
 
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
