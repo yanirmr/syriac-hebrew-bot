93eb1568b822571f6eedf0ad0699b2f3d3db0c32
yanirmr <yanirmr@gmail.com>
2022-01-18 22:44:50 +0200

    change the bot for Heroku deployment

--- bot.py
+++ bot.py
@@ -16,31 +16,20 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 from transliteration import transliterate
 
-PORT = int(os.environ.get('PORT', '8443'))
-TOKEN = os.environ['TELEGRAM_TOKEN']
 
-# Enable logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
-
-logger = logging.getLogger(__name__)
-
-
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('שלום, יש להכניס כאן טקסט בעברית או בסורית')
+    update.effective_message.reply_text('שלום, יש להכניס כאן טקסט בעברית או בסורית')
 
 
 def help(update, context):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    update.effective_message.reply_text('Help!')
 
 
 def transliterate_massage(update, context):
     """Transliterate the the user message from Hebrew to Syriac or vice versa."""
-    update.message.reply_text(transliterate(update.message.text))
+    update.effective_message.reply_text(transliterate(update.effective_message.text))
 
 
 def error(update, context):
@@ -53,32 +42,42 @@ def main():
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater(TOKEN, use_context=True) #your token here
-
+    # Set these variable to the appropriate values	
+    TOKEN = TELEGRAM_TOKEN
+    
+    # Port is given by Heroku	
+    PORT = os.environ.get('PORT')
+    
+    # Enable logging	
+    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',	
+                        level=logging.INFO)	
+    logger = logging.getLogger(__name__)
+    
+    # Set up the Updater	
+    updater = Updater(TOKEN)
     # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # on different commands - answer in Telegram
-    dp.add_handler(CommandHandler("start", start))
+    # Set up the Updater	    
     dp.add_handler(CommandHandler("help", help))
 
     # on noncommand i.e message - transliterate the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, transliterate_massage))
+    dp.add_handler(CommandHandler('start', start))	
+    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, transliterate_massage))
 
     # log all errors
     dp.add_error_handler(error)
 
-    # Start the Bot
+    # Start the webhook
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook('https://hebrew-syriac-bot.herokuapp.com/' + TOKEN)
-
-    # Run the bot until you press Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT. This should be used most of the time, since
-    # start_polling() is non-blocking and will stop the bot gracefully.
+                          url_path=TOKEN,
+                          webhook_url=f"https://{NAME}.herokuapp.com/{TOKEN}")
     updater.idle()
 
 
 if __name__ == '__main__':
     main()
+
+
+
